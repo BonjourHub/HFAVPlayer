4c36ff3766530496f1f224085c22d5e596ac477e
pengfei28 <pengfei28@staff.weibo.com>
2018-06-25 19:51:52 +0800

    解码流程

--- HFAVPlayer/HFAVPlayer/Expand/Readme/README.md
+++ HFAVPlayer/HFAVPlayer/Expand/Readme/README.md
@@ -12,3 +12,29 @@
 [参考]
 http://music-video.cn
 
+
+1. 注册所有容器格式和CODEC:av_register_all()
+
+2. 打开文件:av_open_input_file()
+
+3. 从文件中提取流信息:av_find_stream_info()
+
+4. 穷举所有的流，查找其中种类为CODEC_TYPE_VIDEO
+
+5. 查找对应的解码器:avcodec_find_decoder()
+
+6. 打开编解码器:avcodec_open()
+
+7. 为解码帧分配内存:avcodec_alloc_frame()
+
+8. 不停地从码流中提取出帧数据:av_read_frame()
+
+9. 判断帧的类型，对于视频帧调用:avcodec_decode_video()
+
+10. 解码完后，释放解码器:avcodec_close()
+
+11. 关闭输入文件:av_close_input_file()
+
+https://blog.csdn.net/a2657222/article/details/7566814
+https://blog.csdn.net/leixiaohua1020/article/details/38868499
+
--- HFAVPlayer/HFAVPlayer/ViewController.m
+++ HFAVPlayer/HFAVPlayer/ViewController.m
@@ -8,6 +8,8 @@
 
 #import "ViewController.h"
 
+#import <libavformat/avformat.h>
+
 @interface ViewController ()
 
 @end
@@ -19,6 +21,100 @@
     // Do any additional setup after loading the view, typically from a nib.
     self.view.backgroundColor = [UIColor grayColor];
     
+    [self decode];
+    
+}
+
+- (void)decode {
+    
+    //注册解码器
+    av_register_all();
+    avformat_network_init();
+    
+    //打开文件
+    AVFormatContext *formatContext = avformat_alloc_context();
+    NSString *filePath = [[NSBundle mainBundle] pathForResource:@"test" ofType:@"flv"];
+    if (avformat_open_input(&formatContext, [filePath UTF8String], NULL, NULL) != 0) {
+        HFTODODebugLog(@"打开失败");
+        return;
+    }
+    HFDebugLog(@"打开成功");
+    
+    //从文件中提取流信息
+    if (avformat_find_stream_info(formatContext, NULL) < 0)
+    {
+        HFTODODebugLog(@"没有找到流信息");
+        return;
+    }
+    
+    //穷举所有流，查找需要的流类型
+    int videoIndex = -1;
+    int audioIndex = -1;
+    for (int i = 0; i < formatContext->nb_streams; i++)
+    {
+        enum AVMediaType mediaType = formatContext->streams[i]->codec->codec_type;
+        if (mediaType == AVMEDIA_TYPE_VIDEO)
+        {
+            HFDebugLog(@"发现一路视频流");
+            videoIndex = i;
+            continue;
+        }
+        if (mediaType == AVMEDIA_TYPE_AUDIO)
+        {
+            HFDebugLog(@"发现一路音频流");
+            audioIndex = i;//考虑有多路音频流
+            continue;
+        }
+        HFDebugLog(@"发现一路其他流");
+    }
+    
+    //查找指定类型流的解码器(视频流)
+    AVCodecContext *codecContext = formatContext->streams[videoIndex]->codec;
+    AVCodec *codec = avcodec_find_decoder(codecContext->codec_id);
+    if (codec == NULL) {
+        HFTODODebugLog(@"查找解码器失败");
+        return;
+    }
+    HFDebugLog(@"查找解码器成功");
+    
+    //打开指定的解码器
+    if (avcodec_open2(codecContext, codec, NULL) < 0) {
+        HFTODODebugLog(@"打开解码器失败");
+        return;
+    }
+    HFDebugLog(@"打开解码器成功");
+    
+    //为解码帧分配内存
+    AVFrame *frame = av_frame_alloc();
+    AVFrame *framYUV = av_frame_alloc();
+    
+    //不断的从码流中提取帧数据(子线程)
+    AVPacket *packet = av_malloc(sizeof(AVPacket));
+    if (av_read_frame(formatContext, packet) < 0) {
+        HFTODODebugLog(@"帧数据读取失败 退出子线程");
+        return;
+    }
+    
+    //判断帧类型，调用对应类型解码器解码
+    int gotPicture = -1;
+    if (packet->stream_index == videoIndex) {
+        HFDebugLog(@"视频流类型");
+        if (avcodec_decode_video2(codecContext, frame, &gotPicture, packet) < 0) {
+            HFTODODebugLog(@"解码错误");
+            return;
+        }
+        HFDebugLog(@"解码成功");
+    }
+    
+    //close
+    av_frame_free(&frame);
+    av_frame_free(&framYUV);
+    
+    //解码完成后 释放解码器
+    avcodec_close(codecContext);
+    
+    //关闭输入文件
+    avformat_close_input(&formatContext);
 }
 
 
